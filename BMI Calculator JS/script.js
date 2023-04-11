77549aa358c9128575abc5c7c174f5a1d4f4f842
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2023-04-12 00:24:21 +0600

    Create script.js

---
+++ BMI Calculator JS/script.js
@@ -0,0 +1,42 @@
+var button = document.getElementById('btn');
+button.addEventListener('click', () => {
+    var height = parseInt(document.getElementById('height_id').value);
+    var weight = parseInt(document.getElementById('weight_id').value);
+    var result = document.getElementById('output');
+
+    let height_status = false, weight_status = false;
+    if (height == ' ' || isNaN(height) || (height <= 0)) {
+        document.getElementById('height_error').innerHTML = "Please provide a valid Height";
+    }
+    else {
+        document.getElementById('height_error').innerHTML = "";
+        height_status = true;
+    }
+
+    if (weight == ' ' || isNaN(weight) || (weight <= 0)) {
+        document.getElementById('weight_error').innerHTML = "Please provide a valid Weight";
+    }
+    else {
+        document.getElementById('weight_error').innerHTML = "";
+        weight_status = true;
+    }
+
+    if (height_status && weight_status) {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+        if (bmi < 18.6) {
+            result.innerHTML = "Under Weight: " + bmi;
+        }
+        else if (bmi >= 18.6 && bmi < 24.9) {
+            result.innerHTML = "Normal: " + bmi;
+        }
+        else {
+            result.innerHTML = "Over Weight: " + bmi;
+        }
+    }
+    else {
+        alert("The form has error! Check it.");
+        result.innerHTML = "";
+    }
+
+})
